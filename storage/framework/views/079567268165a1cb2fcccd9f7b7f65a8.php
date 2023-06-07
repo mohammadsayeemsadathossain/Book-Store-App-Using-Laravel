d12b8e9d0c8dc110c9a676a743a279bbddccf546
Mohammad Sayeem Sadat Hossain <96102582+mohammadsayeemsadathossain@users.noreply.github.com>
2023-06-08 04:49:04 +0600

    Add files via upload

---
+++ storage/framework/views/079567268165a1cb2fcccd9f7b7f65a8.php
@@ -0,0 +1,24 @@
+
+<?php $__env->startSection('content'); ?>
+ 
+<div class="card">
+  <div class="card-header">Books Page</div>
+  <div class="card-body">
+      
+      <form action="<?php echo e(url('book')); ?>" method="post">
+        <?php echo csrf_field(); ?>
+
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" class="form-control"></br>
+        <label>Author Name</label></br>
+        <input type="text" name="authorname" id="authorname" class="form-control"></br>
+        <label>Price</label></br>
+        <input type="text" name="price" id="price" class="form-control"></br>
+        <input type="submit" value="Save" class="btn btn-success"></br>
+    </form>
+   
+  </div>
+</div>
+ 
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('books.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel\bookscrud-app\resources\views/books/create.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/1e948e65d090cde73e25b490ea0fc672.php
@@ -0,0 +1,20 @@
+
+<?php $__env->startSection('content'); ?>
+ 
+ 
+<div class="card">
+  <div class="card-header">Books Page</div>
+  <div class="card-body">
+   
+ 
+        <div class="card-body">
+        <h5 class="card-title">Name : <?php echo e($books->name); ?></h5>
+        <p class="card-text">Author Name : <?php echo e($books->authorname); ?></p>
+        <p class="card-text">Price : <?php echo e($books->price); ?></p>
+  </div>
+       
+    </hr>
+  
+  </div>
+</div>
+<?php echo $__env->make('books.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel\bookscrud-app\resources\views/books/show.blade.php ENDPATH**/ ?>
